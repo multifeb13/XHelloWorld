56f25bb88b9dec5b745a3f1aa70baeaa8deaae1e
multifeb13 <musicfair@gmail.com>
2017-01-14 08:02:09 +0900

    ボタン追加

--- HelloWorld/HelloWorld/Page1.xaml.cs
+++ HelloWorld/HelloWorld/Page1.xaml.cs
@@ -13,6 +13,13 @@ namespace HelloWorld
         public Page1()
         {
             InitializeComponent();
+            this.btn1.Clicked += Btn1_Clicked;
         }
+
+        private void Btn1_Clicked(object sender, EventArgs e)
+        {
+            this.text1.Text = "Clicked.";
+        }
+
     }
 }
